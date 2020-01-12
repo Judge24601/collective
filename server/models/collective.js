b3b85cf51d35b5655b808cc39132ad09ceda9d73
Harrison Turley <harrison.turley.1998@gmail.com>
2020-01-11 21:30:48 -0800

    Setting up backend schemas and basic backend endpoints

---
+++ server/models/collective.js
@@ -0,0 +1,26 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var CollectiveSchema = new Schema({
+    title: String,
+    summary: String,
+    notes: String,
+    pollChoices: [
+        {
+            value: String,
+            votes: Number
+        }
+    ],
+    posts: [
+        {
+            id: String
+        }
+    ],
+    users: [
+        {
+            email: String
+        }
+    ]
+});
+
+module.exports = CollectiveSchema;
---
+++ server/models/user.js
@@ -0,0 +1,17 @@
+var mongoose = require("mongoose");
+var Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+    name: String,
+    email: String,
+    customerId: String,
+    collective: String,
+    voted: Boolean,
+    posts: [
+        {
+            id: String
+        }
+    ]
+});
+
+module.exports = UserSchema;
