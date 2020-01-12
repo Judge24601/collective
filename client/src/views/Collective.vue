02094333994823856e78f561ad0c142c3e009b9a
Harrison Turley <harrison.turley.1998@gmail.com>
2020-01-12 04:25:29 -0800

    Add radio buttons

--- client/src/views/Collective.vue
+++ client/src/views/Collective.vue
@@ -5,6 +5,32 @@
       <section class="section-shaped my-1 main">
         <div class="shape shape-style-1 bg-gradient-default"></div>
           <div class="container">
+              <div class="card-cont"> 
+                <card class="border-0" hover shadow body-classes="py-3">
+                  <h3 class="text-primary text-uppercase">Poll</h3>
+                  <div class="card-inner-poll">
+                    <div v-if="!voted" class="vote-options-container">
+                      <div v-for="(option, index) in pollOptions" :key="index" class="vote-option">
+                        <base-radio :name="index" class="mb-3" v-model="radioSelected">
+                          {{option.value}}
+                        </base-radio>
+                      </div>
+                      <div class="vote-buttons-container">
+                        <div class="vote-button">
+                          <base-button type="success" @click="vote">Vote</base-button>
+                        </div>
+                        <div class="add-option-button">
+                          <base-button type="primary" @click="modals.modal1=true">Add poll option</base-button>
+                        </div>
+                      </div>
+                    </div>
+                    <div v-if="voted" class="pie-chart-container">
+                      <h1>other test</h1>
+                    </div>
+                  </div>
+                </card>
+              </div>
+
               <div class="new-post">
                   <base-button type="success" size="lg" iconOnly rounded class="newPost" icon="fa fa-plus" @click="modals.modal0 = true">
                   </base-button>
@@ -61,6 +87,8 @@ import Tabs from "@/components/Tabs/Tabs";
 import TabPane from "@/components/Tabs/TabPane";
 import Modal from "@/components/Modal";
 import PostsService from "@/services/PostsService";
+import CollectiveService from "@/services/CollectiveService";
+import { getInstance } from "@/auth/index";
 export default {
   name: "collective",
   components: {
@@ -74,14 +102,20 @@ export default {
       postReplies: {},
       temp: '',
       modals: {
-        modal0: false
+        modal0: false,
+        modal1: false
       },
       new_title: "",
-      new_description: ""
+      new_poll_option: "",
+      new_description: "",
+      voted: false,
+      pollOptions: [],
+      radioSelected: -1
     };
   },
   mounted() {
     this.getPosts();
+    this.getPollOptions();
   },
   computed: {
     user() {
@@ -98,6 +132,11 @@ export default {
     }
   },
   methods: {
+    async vote() {
+      if (this.radioSelected >= 0) {
+        await CollectiveService.postVote("5e1aa961c9b1c6285c451bf8", this.radioSelected);
+      }
+    },
     async getPosts() {
       const response = await PostsService.fetchPosts(this.user);
       this.posts = response.data.posts;
@@ -127,6 +166,10 @@ export default {
         }
       }
     },
+    async getPollOptions() {
+      const response = await CollectiveService.fetchCollective("5e1aa961c9b1c6285c451bf8");
+      this.pollOptions = response.data.pollChoices;
+    },
     async deletePost(id) {
       await PostsService.deletePost(id);
       this.getPosts();
@@ -137,8 +180,17 @@ export default {
         description: this.new_description,
         collectiveId: this.user.collective
       });
-      this.modals.modal0 = false;
-      this.getPosts();
+      this.modals.modal0 = false
+      this.getPosts()
+    },
+    async addPollOption() {
+      if (this.new_poll_option == "") {
+        return;
+      }
+
+      await CollectiveService.postNewPollOption("5e1aa961c9b1c6285c451bf8", this.new_poll_option);
+      this.modals.modal1 = false;
+      this.getPollOptions();
     }
   }
 };
@@ -158,4 +210,22 @@ export default {
       margin-top: 3.5rem !important;
       margin-bottom: 2rem !important;
     }
+    .vote-option {
+      justify-content: left;
+      text-align: left;
+    }
+    .vote-buttons-container {
+      display: flex;
+      flex-direction: row;
+      justify-content: space-between;
+    }
+    .vote-button {
+      justify-content: left;
+      text-align: left;
+    }
+    .add-option-button {
+      justify-content: right;
+      text-align: right;
+    }
+
 </style>
